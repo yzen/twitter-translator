83a969efc530031641b4a4a41f88d54cf1577d5c
Yura Zenevich <yura.zenevich@gmail.com>
2011-10-19 21:58:40 -0400

    Refactored requestHandler classes

--- src/main.py
+++ src/main.py
@@ -10,47 +10,40 @@ import tornado.httpclient
 import tornado.escape
 import uimodules
 
-class HomeHandler(tornado.web.RequestHandler):
+class AppRequestHandler(tornado.web.RequestHandler):
     def __init__(self, application, request, **kwargs):
         tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
+        #TODO: TEST
         self.lang = self.locale.code[0:2]
+    #TODO: Test
+    def buildRequestUrl(self, urlBase, **params):
+        return urlBase + "?" + urllib.urlencode(params)
 
+class HomeHandler(AppRequestHandler):
     def get(self):
         self.render("html/home.html", tweets=[], lang=self.lang, query="")
 
-class SearchHandler(tornado.web.RequestHandler):
-    def __init__(self, application, request, **kwargs):
-        tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
-        self.lang = self.locale.code[0:2]
-        
+class SearchHandler(AppRequestHandler):
     def get(self):
         self.redirect("/")
 
     @tornado.web.asynchronous
     def post(self):
         query = urlparse.parse_qs(self.request.body)
-        searchUrl = "http://search.twitter.com/search.json?"
-        searchUrl += urllib.urlencode({"q": query["query"][0]})
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(searchUrl, callback=self.on_search)
+        http.fetch(self.buildRequestUrl("http://search.twitter.com/search.json", q=query["query"][0]), callback=self.on_search)
 
     def on_search(self, response):
         if response.error: raise tornado.web.HTTPError(500)
         data = tornado.escape.json_decode(response.body)
         self.render("html/home.html", tweets=data["results"], lang=self.lang, query=data["query"])
         
-class TranslateHandler(tornado.web.RequestHandler):
-    def __init__(self, application, request, **kwargs):
-        tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
-        self.lang = self.locale.code[0:2]
-
+class TranslateHandler(AppRequestHandler):
     @tornado.web.asynchronous
     def post(self):
         query = urlparse.parse_qs(self.request.body)
-        translateUrl = "https://ajax.googleapis.com/ajax/services/language/translate?"
-        translateUrl += urllib.urlencode({"v": "1.0", "q": query["text"][0], "langpair": query["lang"][0] + "|" + self.lang})
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(translateUrl, callback=self.on_translate, headers={"Referer": self.request.headers["Referer"]})
+        http.fetch(self.buildRequestUrl("https://ajax.googleapis.com/ajax/services/language/translate", v="1.0", q=query["text"][0], langpair=query["lang"][0] + "|" + self.lang), callback=self.on_translate, headers={"Referer": self.request.headers["Referer"]})
         
     def on_translate(self, response):
         if response.error: raise tornado.web.HTTPError(500)
