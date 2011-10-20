46ec86c785b295524aede4901db82da29a065c0b
Yura Zenevich <yura.zenevich@gmail.com>
2011-10-20 00:21:04 -0400

    Cleaned up js code

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -1,25 +1,36 @@
 (function ($) {
 
     var translate = function (event) {
-        var button = $(event.currentTarget);
-        var tweet = $(".text", button.parent("p"));
+        // Define the necessary selectors.
+        var button = $(event.currentTarget),
+            paragraph = button.parent("p"),
+            tweet = $(".text", paragraph),
+            lang = $(".lang", paragraph);
+
+        // Hide Translate button after the user requested translation.
         button.hide();
+
+        // Make an ajax request to the server to translate the tweet.
         $.ajax({
             url: "/translate?" + $.param({
                 text: tweet.html(),
-                lang: $(".lang", button.parent("p")).text()
+                lang: lang.text()
             }),
             type: "GET",
             dataType: "json",
             success: function (response) {
+                // Replace tweet's original text with translation.
                 tweet.html(response.translatedText);
             },
-            error: function () {
-                button.show();
-            }
+            // If there was an error during the ajax request show the 
+            // translate button again.
+            error: button.show
         });
     };
 
-    $(".translate").click(translate);
+    $(document).ready(function () {
+        // Bind the translate button click event once the page has been loaded.
+        $(".translate").click(translate);
+    });
     
 })(jQuery);
