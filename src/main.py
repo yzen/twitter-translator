22947bd0f56b7ed21155b8992879f3bfb4d70bc7
Yura Zenevich <yura.zenevich@gmail.com>
2011-10-19 22:30:27 -0400

    Some inheretance inspired refactoring

--- src/main.py
+++ src/main.py
@@ -19,6 +19,21 @@ class AppRequestHandler(tornado.web.RequestHandler):
     def buildRequestUrl(self, urlBase, **params):
         return urlBase + "?" + urllib.urlencode(params)
 
+    #TODO: Test
+    def handleRequestCallback(self, data):
+        """Override in subclass - a handle request callback when making async requests."""
+        return None
+
+    #TODO: Test
+    def wrapCallback(self, response):
+        if response.error: raise tornado.web.HTTPError(500)
+        data = tornado.escape.json_decode(response.body)
+        self.handleRequestCallback(data)
+
+    #TODO: Test
+    def on_resonse(self, response):
+        self.wrapCallback(response)
+
 class HomeHandler(AppRequestHandler):
     def get(self):
         self.render("html/home.html", tweets=[], lang=self.lang, query="")
@@ -31,11 +46,9 @@ class SearchHandler(AppRequestHandler):
     def post(self):
         query = urlparse.parse_qs(self.request.body)
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(self.buildRequestUrl("http://search.twitter.com/search.json", q=query["query"][0]), callback=self.on_search)
+        http.fetch(self.buildRequestUrl("http://search.twitter.com/search.json", q=query["query"][0]), callback=self.on_resonse)
 
-    def on_search(self, response):
-        if response.error: raise tornado.web.HTTPError(500)
-        data = tornado.escape.json_decode(response.body)
+    def handleRequestCallback(self, data):
         self.render("html/home.html", tweets=data["results"], lang=self.lang, query=data["query"])
         
 class TranslateHandler(AppRequestHandler):
@@ -43,11 +56,9 @@ class TranslateHandler(AppRequestHandler):
     def post(self):
         query = urlparse.parse_qs(self.request.body)
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(self.buildRequestUrl("https://ajax.googleapis.com/ajax/services/language/translate", v="1.0", q=query["text"][0], langpair=query["lang"][0] + "|" + self.lang), callback=self.on_translate, headers={"Referer": self.request.headers["Referer"]})
+        http.fetch(self.buildRequestUrl("https://ajax.googleapis.com/ajax/services/language/translate", v="1.0", q=query["text"][0], langpair=query["lang"][0] + "|" + self.lang), callback=self.on_resonse, headers={"Referer": self.request.headers["Referer"]})
         
-    def on_translate(self, response):
-        if response.error: raise tornado.web.HTTPError(500)
-        data = tornado.escape.json_decode(response.body)
+    def handleRequestCallback(self, data):
         self.write(data["responseData"])
         self.finish()
 
@@ -62,7 +73,10 @@ application = tornado.web.Application([
     (r"/translate", TranslateHandler)
 ], **settings)
 
-if __name__ == "__main__":
+def main():
     tornado.locale.load_translations(os.path.join(os.path.dirname(__file__), "translations"))
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
