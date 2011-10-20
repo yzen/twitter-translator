4b8277f8c6e57cf33f4242092ca56abe8341b30f
Yura Zenevich <yura.zenevich@gmail.com>
2011-10-19 23:58:26 -0400

    Added comments

--- src/main.py
+++ src/main.py
@@ -11,12 +11,25 @@ import tornado.escape
 import uimodules
 
 from tornado.options import define, options
+"""Define web application options"""
 define("port", default=8888)
+define("twitterUrl")
+define("googleTranslateUrl")
 
+#TODO: TEST
+"""Build an absolute path to a resource."""
+def getAbsolutePath(relPath):
+    return os.path.join(os.path.dirname(__file__), relPath)
+
+"""
+    tornado.web.Application subclass that implements it with defined settings and 
+    handlers.
+"""
 class WebApplication(tornado.web.Application):
     def __init__(self):
+        """Web application's settings."""
         settings = {
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "static_path": getAbsolutePath("static"),
             "ui_modules": uimodules
         }
         tornado.web.Application.__init__(self, [
@@ -25,61 +38,97 @@ class WebApplication(tornado.web.Application):
             (r"/translate", TranslateHandler)
         ], **settings)
 
+"""
+    tornado.web.RequestHandler's subclass that helps to abstract some of the handler's
+    asynchronous functionality.
+"""
 class AppRequestHandler(tornado.web.RequestHandler):
     def __init__(self, application, request, **kwargs):
         tornado.web.RequestHandler.__init__(self, application, request, **kwargs)
         #TODO: TEST
+        """lang field contains a short version of current user's locale."""
         self.lang = self.locale.code[0:2]
     #TODO: Test
+    """
+        A helper method that builds a full url out of an url base and parameters
+        delivered as keyword arguments.
+    """
     def buildRequestUrl(self, urlBase, **params):
         return urlBase + "?" + urllib.urlencode(params)
 
     #TODO: Test
+    """
+        A callback that's executed after http.fetch when an asynchronous request
+        handler is complete.
+    """
     def handleRequestCallback(self, data):
         """Override in subclass - a handle request callback when making async requests."""
         return None
 
     #TODO: Test
-    def wrapCallback(self, response):
+    """
+        handlerRequestCallback's wrapper that checks for reponse error and parses json 
+        body.
+    """
+    def on_resonse(self, response):
         if response.error: raise tornado.web.HTTPError(500)
         data = tornado.escape.json_decode(response.body)
         self.handleRequestCallback(data)
-
-    #TODO: Test
-    def on_resonse(self, response):
-        self.wrapCallback(response)
-
+"""
+    AppRequestHandler's subclass that handles GET requests to '/' url.
+"""
 class HomeHandler(AppRequestHandler):
     def get(self):
         self.render("html/home.html", tweets=[], lang=self.lang, query="")
 
+"""
+    AppRequestHandler's subclass that handles GET and POST requests to '/search' url.
+"""
 class SearchHandler(AppRequestHandler):
+    """Redirect to base url if the user tries to GET to /search."""
     def get(self):
         self.redirect("/")
 
+    """Handle POST request to '/search' and search twitter for that query string."""
     @tornado.web.asynchronous
     def post(self):
         query = urlparse.parse_qs(self.request.body)
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(self.buildRequestUrl("http://search.twitter.com/search.json", q=query["query"][0]), callback=self.on_resonse)
+        http.fetch(self.buildRequestUrl(options.twitterUrl,
+            q=query["query"][0]),
+            callback=self.on_resonse)
 
+    """Overridden handleRequestCallback that renders twitter search results."""
     def handleRequestCallback(self, data):
         self.render("html/home.html", tweets=data["results"], lang=self.lang, query=data["query"])
-        
+
+"""
+    AppRequestHandler's subclass that handles POST requests to '/translate' url.
+"""
 class TranslateHandler(AppRequestHandler):
+    """Handle POST request to '/translate' and google translate the selected tweet."""
     @tornado.web.asynchronous
     def post(self):
         query = urlparse.parse_qs(self.request.body)
         http = tornado.httpclient.AsyncHTTPClient()
-        http.fetch(self.buildRequestUrl("https://ajax.googleapis.com/ajax/services/language/translate", v="1.0", q=query["text"][0], langpair=query["lang"][0] + "|" + self.lang), callback=self.on_resonse, headers={"Referer": self.request.headers["Referer"]})
-        
+        http.fetch(self.buildRequestUrl(options.googleTranslateUrl,
+            v="1.0", q=query["text"][0], langpair=query["lang"][0] + "|" + self.lang),
+            callback=self.on_resonse,
+            headers={"Referer": self.request.headers["Referer"]})
+
+    """
+        Overridden handleRequestCallback that writes google translate response as 
+        its own response data.
+    """
     def handleRequestCallback(self, data):
         self.write(data["responseData"])
         self.finish()
 
 def main():
-    tornado.locale.load_translations(os.path.join(os.path.dirname(__file__), "translations"))
-    tornado.options.parse_config_file(os.path.join(os.path.dirname(__file__), "app.conf"))
+    """Load translations"""
+    tornado.locale.load_translations(getAbsolutePath("translations"))
+    """Load web app config stored at src/app.conf"""
+    tornado.options.parse_config_file(getAbsolutePath("app.conf"))
     application = WebApplication()
     application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
--- src/uimodules.py
+++ src/uimodules.py
@@ -2,20 +2,40 @@
 
 import tornado.web;
 
+""" Home UIModule that is the base template module for all pages ('/' and '/search')."""
 class Home(tornado.web.UIModule):
+
+    """Load js files."""
     def javascript_files(self):
         return ["lib/jquery-1.6.4.min.js", "js/home.js"]
-        
+
+    """Load css files."""
     def css_files(self):
         return ["css/home.css"]
 
+    """
+        Render Home module using module-home.html template and appropriate data passed as
+        keyword arguments (list of tweets, current user's language and search query string).
+    """
     def render(self, tweets, lang, query):
         return self.render_string("html/uimodules/module-home.html", tweets=tweets, lang=lang, query=query)
 
+""" Tweet UIModule that is the template module for rendering tweet entities."""
 class Tweet(tornado.web.UIModule):
+    """
+        Render Tweet module using module-tweet.html template and appropriate data passed as
+        keyword arguments (tweet information and current user's language).
+    """
     def render(self, tweet, lang):
         return self.render_string("html/uimodules/module-tweet.html", tweet=tweet, lang=lang)
 
+"""
+    Translate UIModule that is the template module for conditional rendering of every
+    tweet's translate button.
+"""
 class Translate(tornado.web.UIModule):
+    """
+        Render Translate (button) module using module-translate.html template.
+    """
     def render(self):
         return self.render_string("html/uimodules/module-translate.html")
